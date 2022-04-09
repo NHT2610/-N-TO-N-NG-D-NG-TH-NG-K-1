119628b32d3ba3d2d278a1b304a3e1f1027baf07
Nguyen Huu Truc <nguyenhuutruc26102002@gmail.com>
2022-04-10 01:23:45 +0700

    last commit

--- 20120608/_20120608.py
+++ 20120608/_20120608.py
@@ -1,14 +1,22 @@
-﻿def Gauss_elimination(A):
+﻿def printMatrix(A):
+    for row in A:
+        for element in row:
+            print(element, end = " ")
+        print()
+
+def Gauss_elimination(A):
     nrow = len(A)
     ncol = len(A[0])
     zeros = 1 #Cờ báo cột đang xét có == 0 hay không
+    pivotRow = -1
     for col in range(ncol - 1):
-        pivotRow = col
+        pivotRow += 1
+        #Kiểm tra cột trước đó có bằng 0 hay không
         if zeros == 1 and col != 0:
             pivotRow = col - 1
         if pivotRow >= nrow:
             pivotRow = nrow - 1
-        #Trường hợp phần tử có cùng chỉ số dòng và cột != 0
+        #Trường hợp phần tử (có cùng chỉ số dòng và cột) != 0
         if A[pivotRow][col] != 0:
             zeros = 0 #Cột này != 0
             #Chia tất cả các phần tử trong dòng col cho phần tử != 0 đầu tiên của dòng
@@ -17,17 +25,17 @@
                 A[pivotRow][i] /= pivot
             #Xét tất cả các phần tử cùng cột col phía dưới
             #Phần tử nào != 0 thì trừ dòng đó cho <hệ số> * dòng col
-            for i in range(pivotRow + 1, nrow):
-                if A[i][col] != 0:
+            for i in range(nrow):
+                if i != pivotRow and A[i][col] != 0:
                     factor = A[i][col]
                     for j in range(col, ncol):
                         A[i][j] -= factor * A[pivotRow][j]
         #Trường hợp phần tử (có cùng chỉ số dòng và cột) == 0
         else:
             index = pivotRow
-            for i in range(pivotRow + 1, nrow):
+            for i in range(nrow):
                 #Thực hiện hoán đổi dòng có phần tử cùng cột col mà != 0 đầu tiên
-                if A[i][col] != 0:
+                if i != pivotRow and A[i][col] != 0:
                     zeros = 0
                     A[pivotRow], A[i] = A[i], A[pivotRow]
                     index = i
@@ -37,13 +45,12 @@
                 pivot = A[pivotRow][col]
                 for i in range(col, ncol):
                     A[pivotRow][i] /= pivot
-                for i in range(index + 1, nrow):
-                    if A[i][col] != 0:
+                for i in range(nrow):
+                    if (i < pivotRow or i >= index + 1) and A[i][col] != 0:
                         factor = A[i][col]
                         for j in range(col, ncol):
                             A[i][j] -= factor * A[pivotRow][j]
             else: zeros = 1
-
 
 def back_substitution(A):
     nrow = len(A)
@@ -64,45 +71,16 @@ def back_substitution(A):
                return
     #Trường hợp hệ có nghiệm duy nhất
     if count == ncol - 1:
-        for i in range(ncol - 2, -1, -1):
-            sum = 0
-            for j in range(ncol - 2, i, -1):
-                sum += A[i][j] * result[j]
-            result[i] = A[i][ncol - 1] - sum
+        for i in range(ncol - 1):
+            result[i] = A[i][ncol - 1];
         #Xuất giá trị nghiệm ra màn hình
         print("Hệ phương trình có nghiệm duy nhất: ")
         for i in range(ncol - 1):
             print("x" + str(i + 1) + " = " + str(result[i]))        
     #Trường hợp hệ có vô số nghiệm
     elif count < ncol - 1:
-        numberOfTemp = ncol - 1 - count
-        coefficients = [[]] * (ncol - 2) #Lưu hệ số các ẩn tự do
-        for i in range(count - 1, -1, -1):
-            index = i
-            for j in range(count - 1, ncol - 1):
-                if A[i][j] != 0:
-                    index = j
-                    break
-            sum = ""    
-            j = index + 1
-            while j < ncol - 2:
-                temp = [0] * (ncol - 1)
-                temp[j] = A[i][j]
-                coefficients[j] = temp
-                result[j] = "a" + str(j)
-                j += 1
-                
-            else:
-                temp = [0] * (ncol - 1)
-                for k in range(index + 1, ncol - 2):
-                    temp[k] = -coefficients[k][k] 
-                    result[i] += " - " + str(abs(coefficients[k][i])) + "a" + str(k + 1)
-                temp[ncol - 2] = A[i][ncol - 1]
-                coefficients[index] = temp    
-            for j in range(index + 1, ncol - 2):
-                result[j] = "a" + str(j)
-                sum += str(result[j])
-            
+        #Tìm nghiệm tự do
+        
         #Xuất giá trị nghiệm ra màn hình
         print("Hệ phương trình có vô số nghiệm có dạng: ")
         for i in range(ncol - 1):
@@ -116,6 +94,9 @@ B = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 C = [[1, 1, 0, 1, 0], [0, 1, 1, 2, 0], [1, 0, 1, 1, 0]]
 D = [[1, 1, 1, 2], [1, 1, 2, 3], [1, 2, 3, 1]]
 E = [[1, 2, 0, 2, 6], [3, 5, -1, 6, 17], [2, 4, 1, 2, 12], [2, 0, -7, 11, 7]]
+F = [[1, 0, 2, 3, 4], [2, 0, -1, 5, 3], [3, 0, 4, 6, -2], [4, 0, 2, 7, 0]]
+G = [[1, 0, 0, 3], [2, 0, 0, 5], [3, 0, 0, 6], [4, 0, 0, 7]]
+H = [[1, 7, 1, 3, 0], [1, 7, -1, -2, -2], [2, 14,2, 7, 0], [6, 42, 3, 13, -3]]
 #print(B)
 #Gauss_elimination(B)
 #print(B)
@@ -131,5 +112,16 @@ E = [[1, 2, 0, 2, 6], [3, 5, -1, 6, 17], [2, 4, 1, 2, 12], [2, 0, -7, 11, 7]]
 #print(E)
 #Gauss_elimination(E)
 #print(E)
+#Gauss_elimination(E)
+#back_substitution(E)
+
+print("Ma trận ban đầu")
+printMatrix(E)
 Gauss_elimination(E)
+print("Ma trận sau khi đưa về dạng bậc thang rút gọn")
+printMatrix(E)
 back_substitution(E)
+
+#Gauss_elimination(H)
+#print(H)
+#back_substitution(H)
